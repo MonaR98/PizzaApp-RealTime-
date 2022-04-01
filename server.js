c4a1f137f601c6bac350892ff7c8220250906728
MonaR98 <mona.r.sharma98@gmail.com>
2022-04-01 12:14:37 +0530

    Register/Login Functionality Implemented

--- server.js
+++ server.js
@@ -6,16 +6,30 @@ const expressLayout = require('express-ejs-layouts');
 const path = require('path')
 
 
+
+//set template engine
+app.use(expressLayout)
+app.set('views', path.join(__dirname, '/resources/views'));
+app.set('view engine', 'ejs');
+
+
 //Assets
 app.use(express.static('public'));
 app.get('/', (req, res) => {
     res.render('home');
 })
 
-//set template engine
-app.use(expressLayout)
-app.set('views', path.join(__dirname, '/resources/views'));
-app.set('view engine', 'ejs');
+app.get('/cart',(req,res)=>{
+    res.render('customers/cart');
+})
+
+app.get('/register',(req,res)=>{
+    res.render('auth/register');
+})
+
+app.get("/login",(req,res)=>{
+    res.render('auth/login');
+})
 
 app.listen(PORT, () => {
     console.log(`Listening on port no ${PORT}`)
